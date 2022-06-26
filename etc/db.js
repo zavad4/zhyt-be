e97b4ceda119e5796b2770fb88a3179161b97bcb
zavad4 <lizavodovska@gmail.com>
2022-06-26 21:24:59 +0300

    update config with deployed db

--- etc/db.js
+++ etc/db.js
@@ -1,11 +1,11 @@
 module.exports = {
-    client: 'mysql',
+    client: 'postgresql',
     connection: {
-      host: '127.0.0.1',
-      port: 3306,
-      user: 'root',
-      password: 'K8XhqonbfRhshyObLg',
-      database: 'zhyt_db',
+      host: 'ec2-34-248-169-69.eu-west-1.compute.amazonaws.com',
+      port: 5432,
+      user: 'cokcicjuysjhxk',
+      password: '021c9aed431a8ed0529b07e59c608cd4208aec4088163d100be1f73702d5a077',
+      database: 'd5hgm981vh1bu7',
     },
     migrations: {
       directory: __dirname + '/../migrations',
